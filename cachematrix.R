20aa3460e87703c551777a74f502b0bfc3579932
Emilia Galdino <emilia_galdino@outlook.com>
2020-08-07 13:27:11 -0300

    Creating the two functions in order to use a cache with the inverse matrix problem.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Function that creates a special matrix to deal with
+## the cache problem
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x 
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get, setsolve = setsolve,
+         getsolve = getsolve)
 
 }
 
 
 ## Write a short comment describing this function
+## Function that checks if there is a value for the
+## inverse matrix in the cache, if so it returns it,
+## else it will calculate a new inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getsolve()
+    if (!is.null(m)) {
+        message("getting matrix from cache")
+        return(m)
+    }
+    data <- x$get() 
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
